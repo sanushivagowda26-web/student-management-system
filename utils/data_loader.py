dfee6c9c46261364e1467e604904971326caffb7
sanushivagowda26-web <sanushivagowda26@gmail.com>
2026-06-04 12:14:09 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+def load_data():
+
+    df = pd.read_csv("data/startup_data.csv")
+
+    df["Startup Age"] = 2026 - df["Year Founded"]
+
+    df["Funding Efficiency"] = (
+        df["Revenue (M USD)"] /
+        df["Funding Amount (M USD)"]
+    )
+
+    df["Revenue Per Employee"] = (
+        df["Revenue (M USD)"] * 1000000
+    ) / df["Employees"]
+
+    df["Valuation Multiple"] = (
+        df["Valuation (M USD)"] /
+        df["Revenue (M USD)"]
+    )
+
+    return df
