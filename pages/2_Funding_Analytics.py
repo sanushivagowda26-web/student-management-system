88f5085de90573c149a16f3f678310a6cfbd06a7
sanushivagowda26-web <sanushivagowda26@gmail.com>
2026-06-04 12:38:32 +0530

    Update 2_Funding_Analytics.py

--- pages/2_Funding_Analytics.py
+++ pages/2_Funding_Analytics.py
@@ -1,1 +1,17 @@
+import plotly.express as px
 
+industry_funding = (
+    df.groupby("Industry")
+    ["Funding Amount (M USD)"]
+    .sum()
+    .reset_index()
+)
+
+fig = px.bar(
+    industry_funding,
+    x="Industry",
+    y="Funding Amount (M USD)",
+    color="Industry"
+)
+
+st.plotly_chart(fig, use_container_width=True)
