557d75863bf0682b763435ef1d0693ebf4ffcc04
sanushivagowda26-web <sanushivagowda26@gmail.com>
2026-06-04 12:44:12 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,23 +1,73 @@
+import streamlit as st
 import pandas as pd
+import numpy as np
 
+
+@st.cache_data
 def load_data():
 
     df = pd.read_csv("data/startup_data.csv")
 
-    df["Startup Age"] = 2026 - df["Year Founded"]
+    current_year = 2026
+
+    # -----------------------------------
+    # Feature Engineering
+    # -----------------------------------
+
+    df["Startup Age"] = (
+        current_year -
+        df["Year Founded"]
+    )
 
     df["Funding Efficiency"] = (
         df["Revenue (M USD)"] /
         df["Funding Amount (M USD)"]
     )
 
-    df["Revenue Per Employee"] = (
-        df["Revenue (M USD)"] * 1000000
-    ) / df["Employees"]
-
     df["Valuation Multiple"] = (
         df["Valuation (M USD)"] /
         df["Revenue (M USD)"]
+    )
+
+    df["Revenue Per Employee"] = (
+        (df["Revenue (M USD)"] * 1_000_000)
+        / df["Employees"]
+    )
+
+    df["Valuation Efficiency"] = (
+        df["Valuation (M USD)"] /
+        df["Funding Amount (M USD)"]
+    )
+
+    # -----------------------------------
+    # Startup Health Score
+    # -----------------------------------
+
+    df["Health Score"] = (
+
+        df["Funding Efficiency"]
+        .rank(pct=True) * 30
+
+        +
+
+        df["Valuation Multiple"]
+        .rank(pct=True) * 30
+
+        +
+
+        df["Revenue Per Employee"]
+        .rank(pct=True) * 20
+
+        +
+
+        df["Market Share (%)"]
+        .rank(pct=True) * 20
+
+    )
+
+    df["Health Score"] = (
+        df["Health Score"]
+        .round(2)
     )
 
     return df
