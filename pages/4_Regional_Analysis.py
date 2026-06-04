3cd7ea0a1a8b1cb42d0d61935eb259823d469d51
sanushivagowda26-web <sanushivagowda26@gmail.com>
2026-06-04 12:42:22 +0530

    Update 4_Regional_Analysis.py

--- pages/4_Regional_Analysis.py
+++ pages/4_Regional_Analysis.py
@@ -1,1 +1,47 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+st.title("🌍 Regional Analysis")
+
+region_stats = (
+    df.groupby("Region")
+    .agg(
+        Funding=("Funding Amount (M USD)","sum"),
+        Revenue=("Revenue (M USD)","sum"),
+        Valuation=("Valuation (M USD)","sum")
+    )
+    .reset_index()
+)
+
+st.dataframe(region_stats)
+
+fig = px.bar(
+    region_stats,
+    x="Region",
+    y="Funding",
+    color="Region",
+    title="Funding by Region"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig = px.sunburst(
+    df,
+    path=["Region","Industry"],
+    values="Funding Amount (M USD)",
+    title="Region vs Industry Funding"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig = px.pie(
+    df,
+    names="Region",
+    title="Startup Regional Distribution"
+)
+
+st.plotly_chart(fig,use_container_width=True)
